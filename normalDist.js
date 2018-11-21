ec22bb4fc673c8724a0b19e464d7360c56567d9c
Nazila <36213959+17nak1@users.noreply.github.com>
2018-11-21 16:02:53 -0500

    Update normalDist.js

--- normalDist.js
+++ normalDist.js
@@ -7,7 +7,6 @@ normal.pnorm = function (x, mu = 0, sd = 1,lower_tail = true, give_log = false) 
   if (sd < 0) {
     return NaN
   }
-
   let ans = 0.5 + 0.5 * erf((x - mu) / Math.sqrt(2) * sd)
   if(!lower_tail) {
      ans = 1- ans 
@@ -17,7 +16,6 @@ normal.pnorm = function (x, mu = 0, sd = 1,lower_tail = true, give_log = false) 
   }
   return ans
 }
-
 normal.rnorm = function (n, mu = 0, sd = 0) {
   if (sd < 0) {
     return NaN
@@ -30,5 +28,4 @@ normal.rnorm = function (n, mu = 0, sd = 0) {
   }
   return arr
 }
-
 module.exports = normal;
