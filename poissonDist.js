bd36857e6dcdbfb60d839876fcf3f9ee685ed4f6
Nazila <36213959+17nak1@users.noreply.github.com>
2019-03-01 15:31:38 -0500

    Update poissonDist.js

--- poissonDist.js
+++ poissonDist.js
@@ -2,24 +2,17 @@
 // lambda represents the rate of occurrence
 var poisson ={}
 let exp = 2.718281828
-poisson.dist = function (lambda = 1, x = 0) {
-  let ans, total = 0
-  if (isNaN(x) || isNaN(lambda) || lambda < 0) {
+poisson.dist = function (lambda = 1) {
+  let k = 0; p = 1; l
+  l = Math.exp(-lambda)
+  if (isNaN(lambda) || lambda < 0) {
     return NaN
   }
-  if (!Number.isInteger(x)) {
-    return 0
+  while (p > l) { 
+    k += 1
+    p = p * Math.random()
   }
-  if (x < 0 || !isFinite(x)) {
-    return 0
-  }
-  x = Math.round(x)
-  ans = -lambda + x * Math.log(lambda)
-  for (let i = 1; i <= x; i++) {
-    total += Math.log(i)
-  }
-  let logAns = ans - total
-  return Math.exp(logAns)
+  return k-1
 }
 
 module.exports = poisson
