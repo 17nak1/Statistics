d43cb072601078ed1fe12e8644bc84037834f4af
Nazila <17nak1@queensu.ca>
2019-09-02 18:33:25 -0400

    clean

--- poissonDist.js
+++ poissonDist.js
@@ -1,18 +1,44 @@
-//* RNG
+//* Probability mass function for the Poisson distribution (pmf)
 // lambda represents the rate of occurrence
 var poisson ={}
 let exp = 2.718281828
-poisson.dist = function (lambda = 1) {
-  let k = 0; p = 1; l
-  l = Math.exp(-lambda)
-  if (isNaN(lambda) || lambda < 0) {
+poisson.dist = function (lambda = 1, x = 0) {
+  let ans, total = 0
+  if (isNaN(x) || isNaN(lambda) || lambda < 0) {
     return NaN
   }
-  while (p > l) { 
-    k += 1
-    p = p * Math.random()
+  if (!Number.isInteger(x)) {
+    return 0
   }
-  return k-1
+  if (x < 0 || !isFinite(x)) {
+    return 0
+  }
+  x = Math.round(x)
+  ans = -lambda + x * Math.log(lambda)
+  for (let i = 1; i <= x; i++) {
+    total += Math.log(i)
+  }
+  let logAns = ans - total
+  return Math.exp(logAns)
 }
 
+function factorial (intValue) {
+  var i, nextNumber, carret, result
+  if (intValue === 0) {
+    return '1'
+  }
+  if (!intValue) {
+    return ''
+  }
+  result = intValue.toString().split('').reverse().map(Number)
+  while (--intValue) {
+    i = carret = 0
+    while ((nextNumber = result[i++]) !== undefined || carret) {
+      carret = (nextNumber || 0) * intValue + carret
+      result[i - 1] = carret % 10
+      carret = parseInt(carret / 10)
+    }
+  }
+  return result.reverse().join('')
+}
 module.exports = poisson
